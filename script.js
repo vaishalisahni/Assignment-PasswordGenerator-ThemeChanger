bfceadab185d5002248ce27f208448f7b0c9ce25
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-04-05 23:20:15 +0530

    made skeleton

---
+++ script.js
@@ -0,0 +1,11 @@
+const generateBtn=document.querySelector(".generate-btn");
+const password=document.querySelector(".password");
+const container=document.querySelector(".password-generator");
+const changeTheme=document.querySelector(".chnge-theme");
+
+generateBtn.addEventListener("click",()=>{
+    for(const i=0;i<8;i++)
+    {
+        
+    }
+})
